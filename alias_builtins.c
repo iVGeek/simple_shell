a61712115c8da7c6cf2a9eed042ee52d0183de39
iVGeek <48223278+iVGeek@users.noreply.github.com>
2022-11-07 18:50:54 +0300

    Removed whitespaces.

--- alias_builtins.c
+++ alias_builtins.c
@@ -1,10 +1,10 @@
 /*
- * File: thevault.c
+ * File: alias_builtins.c
  * Auth: iVGeek
  *      
  */
 
-#include "viking.h"
+#include "shell.h"
 
 int shellby_alias(char **args, char __attribute__((__unused__)) **front);
 void set_alias(char *var_name, char *value);
